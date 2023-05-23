4ac89fe24c47ce16f4664e0b2bf68cb693bd3968
JosepLluisCM <webdevJosep@gmail.com>
2023-05-23 10:50:05 +0200

    importing the full product array from external file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,31 +2,8 @@
 
 
 //____________________MODEL___________________________//
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+
+//we are importing the products array from the data/products.js file
 
 
 //_________________VIEW___________________________//
@@ -87,3 +64,6 @@ products.forEach( (product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+//________________________CONTROLLER________________________//
